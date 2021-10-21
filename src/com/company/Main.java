febc14d91bb1768da4a9b9c18b449e3ffbb0beb6
Pavel <sa1zen@mail.ru>
2021-10-21 20:12:33 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 11,b=12,c=40;
+    if (a>100|| b>3){
+        System.out.println(a);
+    }
+    else {
+        System.out.println(c);
+    }
+    }
+}
